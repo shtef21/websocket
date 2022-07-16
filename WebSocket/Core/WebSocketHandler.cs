c0ab5bef0bd6cb27137b2bcd02e5e417a84084bf
shtef21 <72214442+shtef21@users.noreply.github.com>
2022-07-16 23:05:49 +0200

    simplify

--- WebSocket/Core/WebSocketHandler.cs
+++ WebSocket/Core/WebSocketHandler.cs
@@ -6,7 +6,56 @@ namespace WebSocket.Core
     public class WebSocketHandler
     {
 
-        public static async Task HandleWebSocket (HttpContext context, System.Net.WebSockets.WebSocket socket)
+        public static async Task HandleWebSocket(HttpContext context, System.Net.WebSockets.WebSocket socket)
+        {
+            // 1. Extract useful information from HttpContext
+            string requestRoute = context.Request.Path.ToString();
+            string username = context.Request.Query["username"];
+
+            // Initialize containers for reading
+            bool connectionAlive = true;
+            List<byte> webSocketPayload = new List<byte>(1024 * 4); // 4 KB initial capacity
+            byte[] tempMessage = new byte[1024 * 4]; // Message reader
+
+            // 2. Connection loop
+            while (connectionAlive)
+            {
+                // Empty the container
+                webSocketPayload.Clear();
+
+                // Message handler
+                WebSocketReceiveResult? webSocketResponse;
+
+                // Read message in a loop until fully read
+                do
+                {
+                    // Wait until client sends message
+                    webSocketResponse = await socket.ReceiveAsync(tempMessage, CancellationToken.None);
+
+                    // Save bytes
+                    webSocketPayload.AddRange(new ArraySegment<byte>(tempMessage, 0, webSocketResponse.Count));
+                }
+                while (webSocketResponse.EndOfMessage == false);
+
+                // Process the message
+
+                if (webSocketResponse.MessageType == WebSocketMessageType.Text)
+                {
+                    // 3. Convert textual message from bytes to string
+                    string message = System.Text.Encoding.UTF8.GetString(webSocketPayload.ToArray());
+
+                    Console.WriteLine("Client says {0}", message);
+                }
+                else if (webSocketResponse.MessageType == WebSocketMessageType.Close)
+                {
+                    // 4. Close the connection
+                    connectionAlive = false;
+                }
+
+            }
+        }
+
+        public static async Task HandleWebSocket2 (HttpContext context, System.Net.WebSockets.WebSocket socket)
         {
             // Extract useful information from HttpContext
             string requestRoute = context.Request.Path.ToString();
@@ -50,10 +99,6 @@ namespace WebSocket.Core
                         string message = System.Text.Encoding.UTF8.GetString(webSocketPayload.ToArray());
 
                         Console.WriteLine("Client says {0}", message);
-                    }
-                    else if (webSocketResponse.MessageType == WebSocketMessageType.Binary)
-                    {
-                        // Handle bytes however you need
                     }
                     else if (webSocketResponse.MessageType == WebSocketMessageType.Close)
                     {
